104dcd07f0f737e2d7f76df03d98a8ea3b5e114f
sangwoo-98 <robbyra@gmail.com>
2020-11-08 20:15:40 +0900

    Simplify code + Play!

--- Kotlin/숫자 야구 게임/src/main/kotlin/AppStart.kt
+++ Kotlin/숫자 야구 게임/src/main/kotlin/AppStart.kt
@@ -1,16 +1,9 @@
-import java.lang.IllegalArgumentException
-
 class AppStart {
 
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            val baseballGame = BaseballGame()
-            try {
-                baseballGame.inputNumbers()
-            } catch(exception: IllegalArgumentException) {
-                println("숫자를 잘못 입력하셨습니다.")
-            }
+            BaseballGame.play()
         }
     }
 }
