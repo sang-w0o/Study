8f15ace67c47b7a2c3c5ae4000319cf075de1668
나상우 <robbyra@gmail.com>
2021-01-12 19:38:55 +0900

    Remove unneccessary files.

--- React/React Application/Sports Store/src/data/DataStore.js
+++
@@ -1,7 +0,0 @@
-import {createStore, applyMiddleware} from 'redux';
-import {ShopReducer} from './ShopReducer';
-import {CartReducer} from './CartReducer';
-import {CommonReducer} from './CommonReducer';
-import {asyncActions} from './AsyncMiddleWare';
-
-export const SportsStoreDataStore = createStore(CommonReducer(ShopReducer, CartReducer), applyMiddleware(asyncActions));
