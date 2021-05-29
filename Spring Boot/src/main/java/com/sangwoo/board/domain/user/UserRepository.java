b78e6f1dac66533b1638a0aab5c0acbecddd4d9c
sang-w0o <robbyra@gmail.com>
2021-05-29 23:53:31 +0900

    Remove folder

--- Spring Boot/src/main/java/com/sangwoo/board/domain/user/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sangwoo.board.domain.user;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByEmail(String email);
-}
